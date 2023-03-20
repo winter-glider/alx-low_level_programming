9faef126c912136cc567d76a784873c50072a74b
winter-glider <nyeveratanatswa04@gmail.com>
2023-03-20 15:37:57 -0700

    abcABC

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+  *main- Print alphabet
+  *Return: 0 always
+  */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
