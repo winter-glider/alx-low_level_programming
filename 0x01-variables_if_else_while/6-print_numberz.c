95762fe0d8e28d31b636e68bf7d6b6fa868f12dd
winter-glider <nyeveratanatswa04@gmail.com>
2023-03-20 16:47:02 -0700

    numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+  *main - Print numbers
+  *Return: 0 Always
+  */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		putchar('0' + i);
+	putchar('\n');
+		return (0);
+}
