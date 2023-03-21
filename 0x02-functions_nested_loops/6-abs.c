18d1c0c479d6afe0686788fff9e4851ca3d0c0ce
winter-glider <nyeveratanatswa04@gmail.com>
2023-03-21 16:23:55 -0700

    Abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  *_abs -computes the absolute value of an integer
+  *@int: number
+  *Return: result
+  */
+int _abs(int)
+{
+	int x;
+
+	if (x >= 0 && x <= 0)
+		return (1);
+	else
+		return (0);
+
+}
