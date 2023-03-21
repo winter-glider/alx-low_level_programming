98b480d8fd25bab25d69697cb700c4e7d1377083
winter-glider <nyeveratanatswa04@gmail.com>
2023-03-21 16:39:26 -0700

    race to last

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  *print_last_digit - prints last digit of a number
+  *@n: the number
+  *Return: value of the last digit
+  */
+int print_last_digit(int n)
+{
+	int x;
+
+	n = x % 10;
+
+	return (n);
+
+}
