38896d771d9e79ee499b1044eff290763e233ddf
winter-glider <nyeveratanatswa04@gmail.com>
2023-03-21 15:43:57 -0700

    First try

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  * _isalpha - check for alphabet character
+  *@c: letter lowercase or uppercase
+  *Return: 1 if true , 0 if false
+  */
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+
+		return (1);
+	}
+
+	else if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+}
