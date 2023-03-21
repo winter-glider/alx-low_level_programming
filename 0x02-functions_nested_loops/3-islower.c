25966d1a4ca4a6504eaf1eda05695e6aad047c44
winter-glider <nyeveratanatswa04@gmail.com>
2023-03-21 14:56:47 -0700

    trial 1

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  *_islower - check for lowercase letters
+  *@c: lowercase char to be checked
+  *Return: 1 if true , 0 if false
+  */
+int _islower(int c)
+{
+	int c;
+
+	if (_islower(c))
+
+		return (1);
+	else
+		return (0);
+
+}
