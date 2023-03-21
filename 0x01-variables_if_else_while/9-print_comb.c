e4082a8b1f18cb27222c1ae1f41cd44895ead687
winter-glider <nyeveratanatswa04@gmail.com>
2023-03-20 17:13:39 -0700

    0,1,2,3

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *main - Print possible combo of single digs
+ *Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+		if (n != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
