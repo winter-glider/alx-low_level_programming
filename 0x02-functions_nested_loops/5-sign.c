d39f03fd6b879d3e9f091f3623f307ce75a4d5a2
winter-glider <nyeveratanatswa04@gmail.com>
2023-03-21 16:06:47 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+  *print_sign - prints the sign of a number
+  *@n: the number
+  *Return: 1 if positive ,-1 if negative ,0 if 0
+  */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+
+		return (-1);
+	}
+
+}
