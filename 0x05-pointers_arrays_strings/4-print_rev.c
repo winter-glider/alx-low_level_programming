a911a6a55fa0293a3b664d66ba4e500b166735e4
winter-glider <nyeveratanatswa04@gmail.com>
2023-03-27 15:52:44 -0700

    print_rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+  *print_rev - print string in reverse
+  *@s: the string
+  *Return: nothing
+  */
+void print_rev(char *s)
+{
+	int count = 0;
+	int length;
+
+	while (*s != '\0')
+	{
+		count++;
+		s++;
+
+	}
+
+	s--;
+
+	for (length = count; length > 0 ; length++)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+
+}
