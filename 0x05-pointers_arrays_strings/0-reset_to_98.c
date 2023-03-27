58f06e34a24407eb7b11561fa34233b1df9477a6
winter-glider <nyeveratanatswa04@gmail.com>
2023-03-27 10:37:19 -0700

    Set to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+  *reset_to_98 - updates the value it points to to 98.
+  *@n: a pointer to the int that is being updated to 98
+  *Return: nothing
+  */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
