ee2de8928be5ccdfd30533e21ee3a67c61552123
winter-glider <nyeveratanatswa04@gmail.com>
2023-03-31 13:57:04 -0700

    copy string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+  *_strncpy - copies a string
+  *@dest: destination string
+  *@src: source string
+  *@n: number of bytes to be copied
+  *Return: copied string
+  */
+char *_strncpy(char *dest, char *src, int n)
+{
+	strncpy(dest, src, n);
+
+	return (dest);
+
+}
