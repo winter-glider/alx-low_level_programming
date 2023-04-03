302fd589857cc5db616b4cf6ef12ac189d3364ca
winter-glider <nyeveratanatswa04@gmail.com>
2023-04-03 15:25:34 -0700

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+  *_strchr -  locates a character in a string
+  *@s: The string
+  *@c: the character in the string
+  *Return:first occurrence of the character c in the string s else NULL
+  */
+char *_strchr(char *s, char c)
+{
+	int i;
+	char *ptr = s;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (*ptr == c)
+		{
+			return (ptr);
+		}
+		ptr++;
+
+	}
+
+
+	return (NULL);
+
+}
