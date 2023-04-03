92dbe59f0c76d3268631cb3c6bddef01608cfc23
winter-glider <nyeveratanatswa04@gmail.com>
2023-04-03 16:19:50 -0700

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+
+/**
+  *_strpbrk - searches a string for any of a set of bytes
+  *@s: the string
+  *@accept: the other string
+  *Return: pointer to the byte in s or NULL if not found
+  */
+char *_strpbrk(char *s, char *accept)
+{
+	char *match = strpbrk(s, accept);
+
+	if (*match == strpbrk(s, accept))
+
+		return (match);
+
+	else
+		return (NULL);
+
+}
