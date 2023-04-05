945477d987ca8f060861dd8700cd70286a003ae2
winter-glider <nyeveratanatswa04@gmail.com>
2023-04-05 13:49:53 -0700

    Task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  *_print_rev_recursion - print string in reverse
+  *@s: the string
+  *Return: nothing
+  */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+
+	else
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+
+}
+
+
