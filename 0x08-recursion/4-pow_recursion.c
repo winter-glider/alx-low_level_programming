906aff37be0932326e9bd46efb1b83804dfac4d0
winter-glider <nyeveratanatswa04@gmail.com>
2023-04-05 16:03:14 -0700

    to the power of

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  *_pow_recursion - returns the value of x raised to the power of y
+  *@x: the base
+  *@y:the exponent
+  *Return: -1 if y less than 0 or 1 when 0 else result
+  */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (_pow_recursion(x, y - 1) * x);
+
+	}
+
+}
