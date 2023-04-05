c5be8bd5ffab7949e1ff2818abe2fa983e3bcb01
winter-glider <nyeveratanatswa04@gmail.com>
2023-04-05 13:11:00 -0700

    task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  *_puts_recursion - prints a string, followed by a new line
+  *@s: the string
+  *Return: nothing
+  */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+
+	_putchar('\n');
+
+
+}
