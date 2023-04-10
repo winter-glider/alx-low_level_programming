55f497b521f1bff94f42da32342ab94a15cc083a
winter-glider <nyeveratanatswa04@gmail.com>
2023-04-10 15:06:30 -0700

    mynewnameis

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+  *main - prints its name, followed by a new line
+  *@argc: counts command line arguments
+  *@argv: array pointer to string
+  *Return: 0 always
+  */
+int main( int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+
+}
+
+
