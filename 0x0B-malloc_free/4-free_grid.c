c0deea5f120e49a4c092ae4e37548881fa0d4c57
winter-glider <nyeveratanatswa04@gmail.com>
2023-04-11 16:33:57 -0700

    free_grid

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  *free_grid - frees a 2 dimensional grid
+  *@grid: the grid to be freed
+  *@height: height of grid
+  *Return: nothing
+  */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+
+	}
+
+	free(grid);
+
+}
+
