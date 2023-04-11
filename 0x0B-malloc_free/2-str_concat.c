255b581012ff32d74337a7fff9e0660485fe9e82
winter-glider <nyeveratanatswa04@gmail.com>
2023-04-11 15:00:08 -0700

    strconcat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -25,7 +25,7 @@ char *str_concat(char *s1, char *s2)
 
 	len1 = strlen(s1);
 	len2 = strlen(s2);
-	s3 = malloc(sizeof(char) * (len1 + len2 + 1));
+	s3 = malloc(len1 + len2 + 1);
 
 	if (s3 == NULL)
 	{
