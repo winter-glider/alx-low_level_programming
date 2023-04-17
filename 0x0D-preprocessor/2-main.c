40eb18108f6c4a639c34564bb0cf04eaa6370fc3
winter-glider <nyeveratanatswa04@gmail.com>
2023-04-17 07:33:36 -0700

    Task 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,11 +2,12 @@
 
 /**
   *main - prints the name of the file it was compiled from
+  *
   *Return: 0 always
   */
 int main(void)
 {
-	printf("%s\n", __File__);
+	printf("%s\n", __FILE__);
 
 	return (0);
 
