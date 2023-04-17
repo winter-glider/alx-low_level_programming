f3e35815e0c7bb175b0f712ec9a6f995f9b18103
winter-glider <nyeveratanatswa04@gmail.com>
2023-04-17 07:26:53 -0700

    main.c

---
+++ 0x0D-preprocessor/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+  *main - prints the name of the file it was compiled from
+  *Return: 0 always
+  */
+int main(void)
+{
+	printf("%s\n", _ _File_ _);
+
+	return (0);
+
+}
