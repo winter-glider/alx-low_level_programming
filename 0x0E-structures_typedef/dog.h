96247c6294bad3899c8ebb093f9add9cbf4c8f9d
winter-glider <nyeveratanatswa04@gmail.com>
2023-04-17 13:03:46 -0700

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+  *struct dog - Define a new type struct dog
+  *@name: struct member 1
+  *@age: struct member 2
+  *@owner: struct member 3
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+
+};
+
+
+#endif
