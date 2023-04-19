96d4016b629f7f62dc170d410954bc504b823fa5
winter-glider <nyeveratanatswa04@gmail.com>
2023-04-19 07:26:17 -0700

    Task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+  *print_name - prints a name
+  *@name: first operand
+  *@f: pointer to a function
+  *Return: nothing
+  */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+
+}
