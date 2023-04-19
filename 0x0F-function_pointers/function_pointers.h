331deea4e810c58908c025a521aa6a9bd587c98c
winter-glider <nyeveratanatswa04@gmail.com>
2023-04-19 07:49:01 -0700

    Task 0

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,8 @@
-#include <stddef.h>
 #ifndef _FUNCTION_POINTERS_H_
 #define _FUNCTION_POINTERS_H_
+
+#include <stddef.h>
+#include <stdlib.h>
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
