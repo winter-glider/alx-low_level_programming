b9192b5a32e5e8af5ef27d179c6aee62d886b1f5
winter-glider <nyeveratanatswa04@gmail.com>
2023-04-20 10:31:52 -0700

    TASK 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -9,42 +9,43 @@
   */
 void print_all(const char * const format, ...)
 {
-	va_list args;
-	const char *k;
-	int i;
-	char x, *s;
-	float f;
-
-	va_start(args, format);
-	k = format;
-
-	while (*k)
 	{
-		switch (*k)
+	int i = 0;
+	char *str, *sep = "";
+	va_list list;
+
+	va_start(list, format);
+	if (format)
+	{
+		while (format[i])
 		{
-			case 'i':
-				i = va_arg(args, int);
-				printf("%d", i);
+			switch (format[i])
+			{
+			case 'c':
+				printf("%s%c", sep, va_arg(list, int));
 				break;
-			case 'x':
-				x = va_arg(args, int);
-				printf("%c", x);
+			case 'i':
+				printf("%s%d", sep, va_arg(list, int));
 				break;
 			case 'f':
-				f = va_arg(args, double);
-				printf("%f", f);
+				printf("%s%f", sep, va_arg(list, double));
 				break;
 			case 's':
-				s = va_arg(args, char *);
-				if (s != NULL)
-					printf("%s", s);
-				else
-					printf("(nil)");
+				str = va_arg(list, char *);
+				if (!str)
+					str = "(nil)";
+				printf("%s%s", sep, str);
 				break;
 			default:
-				break;
+				i++;
+				continue;
+			}
+			sep = ", ";
+			i++;
 		}
 	}
 	printf("\n");
-	va_end(args);
+	va_end(list);
+	}
 }
+
