1e82cd480cdb383105502e626054674c220071da
winter-glider <nyeveratanatswa04@gmail.com>
2023-04-20 08:49:25 -0700

    TASK 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -16,6 +16,9 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	va_start(args, n);
 
+	if (n == 0)
+		return;
+
 	for (i = 0; i < n; i++)
 	{
 		x = va_arg(args, const char *);
