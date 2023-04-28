15ed21ce36709ca655cf2034526e9e6243cce7ad
winter-glider <nyeveratanatswa04@gmail.com>
2023-04-28 14:26:08 -0700

    Task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+  *print_list -prints all the elements of a list_t list
+  *@h: the list
+  *Return:the number of nodes
+  */
+size_t print_list(const list_t *h)
+{
+	size_t temp = 0;
+
+	while (h)
+	{
+		if (h->str != NULL)
+		{
+			printf("[0] (nil)");
+			return (temp);
+		}
+
+		else
+		{
+			printf("[%u] %s", h->len, h->str);
+			h = h->next;
+			temp++;
+		}
+
+	}
+
+	return (temp);
+
+}
+
+
+
