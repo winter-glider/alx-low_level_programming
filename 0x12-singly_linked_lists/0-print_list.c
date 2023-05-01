76ca6a358656e02b96b07682d21b66eb2de0eda5
winter-glider <nyeveratanatswa04@gmail.com>
2023-05-01 06:44:11 -0700

    update Task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,12 +12,15 @@ size_t print_list(const list_t *h)
 
 	while (h != NULL)
 	{
-		if (!h->str)
+		if (h->str == NULL)
 		{
-			printf("[0] (nil)");
+			printf("[0] (nil)\n");
 		}
 
-		printf("[%u] %s\n", h->len, h->str);
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
 		h = h->next;
 		temp++;
 	}
