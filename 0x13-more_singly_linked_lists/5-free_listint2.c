f7f0ae1d826068821182bd54e0e0503b4a39ab9d
winter-glider <nyeveratanatswa04@gmail.com>
2023-05-02 14:51:51 -0700

    task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+  *free_listint2 - frees listint_t
+  *@head: head of list
+  *Return: nothing
+  */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (*head)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+	}
+
+	*head = NULL;
+
+}
