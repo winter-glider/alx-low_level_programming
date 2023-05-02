aaba24a21ae6779230bfb2162a2374200da3d056
winter-glider <nyeveratanatswa04@gmail.com>
2023-05-02 13:26:20 -0700

    Task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+  *print_listint - prints listint_t list
+  *@h: head of list
+  *Return: the number of nodes
+  */
+size_t print_listint(const listint_t *h)
+{
+	int count = 0;
+
+	while (h)
+	{
+
+		printf("%d\n", count);
+
+		h = h->next;
+		count++;
+
+	}
+
+	return (count);
+
+}
