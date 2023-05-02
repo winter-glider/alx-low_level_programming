d5f21294ad156fb65160eb3f14b2f86876075bbd
winter-glider <nyeveratanatswa04@gmail.com>
2023-05-02 13:38:51 -0700

    task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,11 +9,9 @@ size_t print_listint(const listint_t *h)
 {
 	int count = 0;
 
-	while (h)
+	while (h != NULL)
 	{
-
-		printf("%d\n", count);
-
+		printf("%d\n", h->n);
 		h = h->next;
 		count++;
 
