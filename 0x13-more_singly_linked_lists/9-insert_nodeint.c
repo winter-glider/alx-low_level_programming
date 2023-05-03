114f986442a74719c42bd4ed4fdda370d1cfae36
winter-glider <nyeveratanatswa04@gmail.com>
2023-05-02 17:29:52 -0700

    Task 9

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,48 @@
+#include "lists.h"
+
+/**
+  *insert_nodeint_at_index - inserts node
+  *@head: head of list
+  *@idx: where node is placed
+  *@n: integer
+  *Return: the address of the new node, or NULL if it failed
+  */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *ptr, *prev;
+	unsigned int i;
+
+	if (*head == NULL)
+		return (NULL);
+
+	ptr = malloc(sizeof(listint_t));
+	if (ptr == NULL)
+		return (NULL);
+
+	ptr->n = n;
+	ptr->next = NULL;
+
+	if (idx == 0)
+	{
+		ptr->next = *head;
+		*head = ptr;
+		return (ptr);
+	}
+	prev = *head;
+
+	for (i = 0; i < idx; i++)
+	{
+		if (prev->next == NULL)
+		{
+			free(ptr);
+			return (NULL);
+		}
+
+		prev = prev->next;
+	}
+
+	ptr->next = prev->next;
+	prev->next = ptr;
+
+	return (ptr);
+}
